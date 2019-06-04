0e9696551f1c486eff22b58155d6a85f6e43161e
wangpan <wangpan@laiye.com>
2019-06-04 23:14:00 +0800

    feat(Src): add xhr file

--- src/ts-axios.ts
+++
@@ -1,8 +0,0 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {
-  constructor() {
-    console.log(11111)
-  }
-}
